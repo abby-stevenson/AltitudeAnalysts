8331d159e7fc9574ff0ed59effafaf929bc0d07e
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-04-08 23:08:34 -0400

    Put Elevator Pitch as About Page

--- app/src/pages/40_About.py
+++ app/src/pages/40_About.py
@@ -8,11 +8,21 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    We are building an app that streamlines flight logistics for travel agents, passengers, airline managers, and airport managers. 
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
+Our data-driven approach to this solution will benefit these entities by allowing for a more personalized experience for users; 
+both travel agents and passengers will be able to filter flights based on prices and locations. This will also help improve efficiency
+ because all of the flight information, including departure times, arrival times, and prices, will all be in one place. The pain point
+   we are addressing is mainly airports and passengers have to communicate with each airline separately. Since passengers don’t 
+   constantly fly with the same airline and the airports have to communicate with all the airlines flying from and to the airport; 
+   it can be time consuming and tiresome for them. Our app will collect, store and analyze all the data in one place making the 
+   process more efficient and easier for passengers, airports and airlines.
 
-    Stay tuned for more information and features to come!
+Our app will provide airline and airport managers abilities to see graphs and statistics on the flights from their airline and leaving
+ their airport respectively. It will also provide the functionality for passengers to see the status of their flight and information 
+ pertaining to their airport experience. Finally it will give the airline manager the ability to add and remove flights that they are 
+ operating. 
+
+
     """
         )
