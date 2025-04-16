98d0f558e38fd3b7b698c03e643cd003637c5bdd
EmilyF10 <ferry.em@northeastern.edu>
2025-04-15 22:46:02 -0400

    Moniter Flights
    Moniter Flights UI

--- app/src/pages/33_Monitor_Flights.py
+++ app/src/pages/33_Monitor_Flights.py
@@ -1,1 +1,31 @@
+import streamlit as st
+st.set_page_config(layout = 'wide')
+import logging
+logger = logging.getLogger(__name__)
+import requests
+from streamlit_extras.app_logo import add_logo
+from modules.nav import SideBarLinks
+from datetime import datetime
+SideBarLinks()
+
+
+st.write("# Update the Gate for Flights")
+
+Flight_input = st.text_input("Enter Flight Number:", key="FlightNumber")
+Gate_input = st.text_input("Enter New Gate Number:", key="GateNumber")
+
+if st.button("Change Gate"):
+    try:
+        response = requests.put(f"http://api:4000/z/flights/{Flight_input}/{Gate_input}")
+
+        if response.status_code == 200:
+            st.success("Gate updated successfully!")
+        else:
+            st.warning(f"Failed to update gate. Status code: {response.status_code}")
+
+    except Exception as e:
+        st.warning(f"Error: {e}")
+
+
+
 
